d987e126bafe26904a19eefd384bedb5daa468c2
americanman <snowboardracer7@gmail.com>
2018-12-13 11:58:27 -0500

    Add a PassCallbackAsProp component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Controllerwillreceiveprops from './components/Willreceiveprops/'
+import FunctionAsProp from './components/PassCallbackAsProp/'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         First line of the page
         <Controllerwillreceiveprops   />
+        <FunctionAsProp />
       </div>
     );
   }
