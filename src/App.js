6fe15185d7226710f08c97f430d04c23e0ecad94
americanman <snowboardracer7@gmail.com>
2018-12-12 22:56:35 -0500

    Reorganize folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Controllerwillreceiveprops from './components/Willreceiveprops'
+import Controllerwillreceiveprops from './components/Willreceiveprops/'
 
 class App extends Component {
   render() {
