65f36cc267498ea57b6b766443779d814f9391d7
americanman <snowboardracer7@gmail.com>
2018-12-12 22:51:29 -0500

    Add WillReceiveProps() lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Controllerwillreceiveprops from './components/Willreceiveprops'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        First line
+        First line of the page
+        <Controllerwillreceiveprops   />
       </div>
     );
   }
--- src/components/Willreceiveprops.js
+++ src/components/Willreceiveprops.js
@@ -1,1 +1,55 @@
+import React from 'react';
 
+class Willupdateprops extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillUpdate() {
+    console.log('Component is about to update...');
+  }
+  // change code below this line
+  componentWillReceiveProps(nextProps) {
+     console.log(this.props)
+     console.log(nextProps)
+  }
+   
+   componentDidUpdate() {
+     console.log("it did updated")
+   }
+
+  // change code above this line
+  render() {
+    return <h1>{this.props.message}</h1>
+  }
+};
+
+class Controllerwillupdateprops extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'First Message'
+    };
+    this.changeMessage = this.changeMessage.bind(this);
+  }
+  changeMessage() {
+    this.setState({
+      message: 'Second Message'
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>componentWillReceiveProps()</h1>
+        <p> componentDidUpdate(), and is called immediately after a component re-renders.<br />
+            Note that rendering and mounting are considered different things in the component<br />
+            lifecycle. When a page first loads, all components are mounted and this is where methods like<br /> 
+            componentWillMount() and componentDidMount() are called. After this, as state changes, components re-render themselves.
+        </p>
+        <button onClick={this.changeMessage}>Update</button>
+        <Willupdateprops message={this.state.message}/>
+      </div>
+    );
+  }
+};
+
+export default Controllerwillupdateprops
