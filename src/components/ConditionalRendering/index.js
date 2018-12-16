eb4ba201be34aceb8113d92e8f58201fad271e10
americanman <snowboardracer7@gmail.com>
2018-12-16 17:47:17 -0500

    Add ternary operator to conditional rendering

--- src/components/ConditionalRendering/index.js
+++ src/components/ConditionalRendering/index.js
@@ -25,15 +25,27 @@ class ConditionalRendering extends React.Component {
     //    else {
     //      return (
     //        <div>
-    //          <button onClick={this.toggleDisplay}>Toggle Display</button>       </div>
+    //          <button onClick={this.toggleDisplay}>Toggle Display</button>
+    //        </div>
     //      );
     //    }
     //
-    // instead of if/else we can use short-circuit
+    //   We can use ternary but in this case we have to return else after semicolon
+    //   it can be either false or null 
+    //   return (
+    //     <div>
+    //       <button onClick={this.toggleDisplay}>Toggle Display</button>
+    //       {this.state.display ? (<h1>Displayed with ternary!</h1>) : null}
+    //     </div>
+    //   )
+    //
+    // instead of if/else we can use short-circuit and we don't need to describe
+    // else to return nothing
+
     return (
       <div>
         <button onClick={this.toggleDisplay}>Toggle Display</button>
-        {this.state.display && (<h1>Displayed!</h1>)}
+        {this.state.display && (<h1>Displayed !</h1>)}
       </div>
     )
   }
