82c161e3e57d8a6f3acc240a67ddbe207ca166cc
americanman <snowboardracer7@gmail.com>
2018-12-16 10:50:16 -0500

    Add MagicEightBall game component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Controllerwillreceiveprops from './components/Willreceiveprops/'
 import FunctionAsProp from './components/PassCallbackAsProp/'
+import ShouldComponentUpdateControler from './components/ShouldComponentUpdate/'
+import MagicEightBall from './components/MagicEightBall/'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         First line of the page
-        <Controllerwillreceiveprops   />
         <FunctionAsProp />
+        <Controllerwillreceiveprops   />
+        <ShouldComponentUpdateControler />
+        <MagicEightBall />
       </div>
     );
   }
